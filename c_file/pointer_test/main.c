9dfc21f463f4efa3d83e39ad00606d1ef166d8ed
guopenghui <guopenghui@sis.sh.cn>
2025-07-11 06:34:37 +0000

    更新函数指针用法

--- c_file/pointer_test/main.c
+++ c_file/pointer_test/main.c
@@ -4,6 +4,14 @@
 #include <stdbool.h>
 #include <assert.h>
 #define MAX_STUDENT_NUM 100
+
+enum arithmetic{
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+    ARI_MAX
+};
 typedef struct  _name_t
 {
     int max_len;
@@ -27,17 +35,16 @@ int student_init(student_t *student,int age,char *name)
     assert(name != NULL);
     student->name.data = name; 
 }
+
+int add(int a, int b){return a + b;}
+int subtract(int a, int b){return a - b;}
+int multiply(int a, int b){return a*b;}
+int divide(int a, int b){return a/b;};
+
 int main()
 {
-
-    for(int i = 0;i<50;i++ )
-    {
-        printf("%d ",i);
-    }
-    printf("\n 50 \n");
-    for(int i = 51;i<101;i++ )
-    {
-        printf("%d ",i);
-    }
+    int a = 10, b = 5;
+    int (*haha[ARI_MAX])(int ,int) = {add, subtract, multiply, divide};
+    printf("a = %d, s = %d, m = %d, d = %d\n",haha[ADD](a,b),haha[SUBTRACT](a,b),haha[MULTIPLY](a,b),haha[DIVIDE](a,b));
     return 0;
 }
