082117862aa2ca07a48c424fe8c4cd8914de82e0
guoxixi <1438043840@qq.com>
2026-04-16 19:39:49 +0800

    haha

---
+++ cpp_file/list_test/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <list>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+using namespace std;
+
+class car {
+public:
+    car(string n, string c) : m_name(n), color(c) {}
+    string m_name;
+    string color;
+    void print() {
+        cout << "name: " << m_name << ", color: " << color << endl;
+    }
+};
+
+list<function<void()>> tasks;
+mutex mtx;
+condition_variable cv;
+bool running = true;
+
+int main() {
+    car car1("car1", "red");
+    car car2("car2", "blue");
+    car car3("car3", "green");
+
+    /* 消费者线程 */
+    thread worker([&]() {
+        while (running) {
+            function<void()> task;
+
+            {
+                unique_lock<mutex> lock(mtx);
+                cv.wait(lock, [] {
+                    return !tasks.empty() || !running;
+                });
+
+                if (!running && tasks.empty())
+                    return;
+
+                task = move(tasks.front());
+                tasks.pop_front();
+            }
+
+            task();  // 在锁外执行
+        }
+    });
+
+    /* 生产者：初始任务 */
+    {
+        lock_guard<mutex> lock(mtx);
+        tasks.push_back([&car1] { car1.print(); });
+        tasks.push_back([&car2] { car2.print(); });
+        tasks.push_back([&car3] { car3.print(); });
+    }
+    cv.notify_one();
+
+    /* 持续生产 */
+    for (int i = 0; i < 10; ++i) {
+        this_thread::sleep_for(chrono::seconds(1));
+        {
+            lock_guard<mutex> lock(mtx);
+            tasks.push_back([i] {
+                cout << "car " << i << " is printing\n";
+            });
+        }
+        cv.notify_one();
+    }
+
+    /* 优雅退出 */
+    {
+        lock_guard<mutex> lock(mtx);
+        running = false;
+    }
+    cv.notify_all();
+
+    worker.join();
+    return 0;
+}
